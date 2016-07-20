541185c4d1a6232caa6def84f7bfeec90f72c675
Anna Rankin <anna@annarankin.com>
2016-07-20 15:37:33 -0700

    hello-world react setup

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import styles from './styles/style.css'
 import React, { Component } from 'react'
+import ReactDOM, { render } from 'react-dom'
+import GameBoard from './components/game_board.js'
+
+render(<GameBoard/>, document.getElementById('game-board'))
---
+++ src/components/game_board.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class GameBoard extends Component {
+  render(){
+    return <h1>this is a gameboard</h1>
+  } 
+}
